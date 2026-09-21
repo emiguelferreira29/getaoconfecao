666efeb9a2bb33a6ae972362e9ea9dd8fd22c635
emiguelferreira29 <e.miguel.ferreira29@gmail.com>
2026-09-21 14:50:13 +0100

    erro supabase

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lsgwqllgeretvagizgio.supabase.co/rest/v1/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzZ3dxbGxnZXJldHZhZ2l6Z2lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5ODU4MjMsImV4cCI6MjEwNTU2MTgyM30.6TMf80gNXXcsPpbsd4G-71zitbiQ1j8JNh8yD5DxDas';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
